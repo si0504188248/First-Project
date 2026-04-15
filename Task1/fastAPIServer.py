da8ae875a46605e62100ef6ce00eeff61bf1fe65
hadas tapiro <hadastapiro@gmail.com>
2026-04-15 11:54:16 +0300

    fix: restore fastAPIServer and update logic to support list of users

---
+++ Task1/fastAPIServer.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import json
+import os
+
+app = FastAPI()
+FILE_NAME = "DB_FILE.json"
+
+class User(BaseModel):
+    user_id: int
+    username: str
+
+@app.post("/add")
+async def add_payload(user: List[User]):
+    with open(FILE_NAME, "a") as f:
+        for u in user:
+            f.write(u.model_dump_json() + "\n")
+    return {"status": "success", "added": user}
+
+@app.get("/history")
+async def get_last_payloads():
+    if not os.path.exists(FILE_NAME):
+        return "Didnt found"
+
+    with open(FILE_NAME, "r") as f:
+        lines = f.readlines()
+
+    last_10 = [json.loads(line) for line in lines[-10:] if line.strip()]
+    return last_10
