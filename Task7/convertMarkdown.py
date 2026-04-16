37880e92d8ec7d688fa9d51752aca708fd08688d
hadas tapiro <hadastapiro@gmail.com>
2026-04-16 07:47:47 +0300

    Add solution for task 7

---
+++ Task7/convertMarkdown.py
@@ -0,0 +1,26 @@
+from markdown_it import MarkdownIt
+from xhtml2pdf import pisa
+from io import BytesIO
+import os
+
+md = MarkdownIt()
+
+path = input("enter the path of your file: ").strip()
+
+if not os.path.isfile(path):
+    print("File not found")
+    exit()
+
+with open(path, "r", encoding="utf-8") as f:
+    content = f.read()
+
+html_content = md.render(content)
+
+pdf_output = BytesIO()
+
+pisa.CreatePDF(html_content, dest=pdf_output, encoding="utf-8")
+
+with open("output.pdf", "wb") as f:
+    f.write(pdf_output.getvalue())
+
+print("PDF created successfully")
