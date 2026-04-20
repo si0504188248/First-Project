1345888a118c33c86fcd29f9d3915008db38a57d
hadas tapiro <hadastapiro@gmail.com>
2026-04-20 11:48:45 +0300

    🐛 Improvment version of Task 7 includes: Key Improvements:
    Modular: Split into open_file and md_to_pdf for reusability.
    
    Safe: Comprehensive try-except blocks to prevent crashes.
    
    Validated: Added path verification and PDF status checks.
    
    Compatible: Patched hashlib to fix environment-specific bugs.

--- Task7/convertMarkdown.py
+++ Task7/convertMarkdown.py
@@ -1,20 +1,43 @@
+
 from markdown_it import MarkdownIt
 from xhtml2pdf import pisa
-from io import BytesIO
-import os
 
-# final version
-md = MarkdownIt()
-path = input("enter the path of your file: ").strip()
-if not os.path.isfile(path):
-    print("File not found")
-    exit()
-with open(path, "r", encoding="utf-8") as f:
-    content = f.read()
-html_content = md.render(content)
-with open("output.pdf", "wb") as f:
-    pisa.CreatePDF(html_content, dest=f)
-print("PDF created successfully")
+
+def open_file(file,mode):
+    try:
+        with open(file, mode,encoding="utf-8") as f:
+            return f.read()
+    except Exception as e:
+        print(f"Reading error occurred: {e}")
+        return None
+
+def md_to_pdf(content):
+    md = MarkdownIt()
+    html_content = md.render(content)
+    try:
+        with open("output.pdf", "wb") as f:
+            status = pisa.CreatePDF(html_content, dest=f, encoding="utf-8")
+            if status.err == 0:
+                print("PDF created successfully")
+            else:
+                print(f"PDF creation failed with error code: {status.err}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    while True:
+        path = input("enter path of your file: ").strip()
+        file_content = open_file(path, "r")
+        if file_content is not None:
+            break
+        print("invalid path, please try again.\n")
+    md_to_pdf(file_content)
+
+
+
+
+
 
 
 
